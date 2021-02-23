1556bdb722d3f8502ab67130d8130c064ec09b78
Georgiy Tonkiy <georgiy.tonkiy@gmail.com>
2021-02-23 11:02:16 +0200

    12345

--- js/header-menu.js
+++ js/header-menu.js
@@ -1,14 +1,30 @@
+// (() => {
+//   const refs = {
+//     openModalBtn: document.querySelector('[header-menu-open]'),
+//     modal: document.querySelector('[header-menu]'),
+//     modal2: document.querySelector('[close-menu]'),
+//   };
+
+//   refs.openModalBtn.addEventListener('click', toggleModal);
+
+//   function toggleModal() {
+//       refs.modal.classList.toggle('is-hidden');
+//       refs.modal2.classList.toggle('is-open');
+//   }
+// })();
+
+
 (() => {
   const refs = {
-    openModalBtn: document.querySelector('[header-menu-open]'),
-    modal: document.querySelector('[header-menu]'),
-    modal2: document.querySelector('[close-menu]'),
+    openModalBtn: document.querySelector('[mobile-menu-open]'),
+    closeModalBtn: document.querySelector('[mobile-menu-close]'),
+    modal: document.querySelector('[mobile-modal]'),
   };
 
   refs.openModalBtn.addEventListener('click', toggleModal);
+  refs.closeModalBtn.addEventListener('click', toggleModal);
 
   function toggleModal() {
-      refs.modal.classList.toggle('is-hidden');
-      refs.modal2.classList.toggle('is-open');
+    refs.modal.classList.toggle('is-hidden');
   }
 })();
